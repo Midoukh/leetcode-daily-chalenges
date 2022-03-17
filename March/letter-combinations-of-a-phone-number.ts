59182b1bc815733b6be509134272c54cc92c37d7
ahmed khelili <ahmedkhelili132@gmail.com>
2022-03-17 16:57:39 +0100

    17/03/2022 2

--- March/letter-combinations-of-a-phone-number.ts
+++ March/letter-combinations-of-a-phone-number.ts
@@ -21,21 +21,18 @@ const letterCombinations = (digits: string): string[] => {
   //285 ==> ['2', '8', '5']
   // 2 ==> abc,  8 ==> tuv,  5 ==> jkl
   // atj auk avl btj ...
-  console.log(Number(digits[0]));
+
   const letters: string[] = [];
   const first: string[] = map[Number(digits[0])].split("");
   const second: string[] = letterCombinations(
     digits.substring(1, digits.length)
   );
-  console.log("first", first);
-  console.log("second", second);
 
   for (let i = 0; i < first.length; i++) {
     for (let j = 0; j < second.length; j++) {
       letters.push(first[i] + second[j]);
     }
   }
-  console.log(letters);
   return letters;
 };
 console.log(letterCombinations("284"));
